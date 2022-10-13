0d660169dec7d1d0b6d58e0f7df6cdb257752ed8
Giovanna Linda <eugiovannasouza@gmail.com>
2022-10-13 17:27:43 -0300

    feat: importing tokens

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import './styles/global.css'
 
 export function App() {
   return (
-    <h1 className='flex justify-center text-violet-600 font-bold'>Hello World</h1>
+   <>
+      <h1 className='flex justify-center text-gray-900 text-2xl font-bold'>Hello World</h1>
+      <button className='bg-cyan-700 rounded px-4 py-2 hover:bg-cyan-500 text-gray-900'>Enviar</button>
+   </>
   )
 }
