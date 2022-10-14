cd9912858b19892f9749914b7deef4d217c78389
Giovanna Linda <eugiovannasouza@gmail.com>
2022-10-13 21:55:32 -0300

    feat: heading component

---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react'
+import { Slot } from '@radix-ui/react-slot'
+import { clsx } from 'clsx'
+
+export type HeadingProps = {
+  size?: 'sm' | 'md' | 'lg' 
+  children: ReactNode
+  asChild?: boolean
+}
+
+export function Heading({ children, asChild, size = 'md' }: HeadingProps) {
+  const Component = asChild ? Slot : 'h2'
+
+  return (
+    <Component
+      className={clsx('text-gray-100 font-bold', {
+        'text-lg': size === 'sm',
+        'text-xl': size === 'md',
+        'text-2xl': size === 'lg',
+      })}
+    >
+      {children}
+    </Component>
+  )
+}
